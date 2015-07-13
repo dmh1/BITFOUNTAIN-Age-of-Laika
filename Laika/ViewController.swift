f7bc932c11d9f55b37fd973bb93de6dbbc8a23e9
Dallin Holder <dallinholder@Dallins-MacBook-Pro.local>
2015-07-13 12:46:27 +0800

    Initial Commit

---
+++ Laika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Laika
+//
+//  Created by Dallin Holder on 13/07/2015.
+//  Copyright (c) 2015 Dallin Holder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
